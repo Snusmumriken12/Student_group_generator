826f3b9cb83a4fcf3e429bf8ade85b2f9b241c99
Linus Gustafsson <linus.gustaf01@gmail.com>
2026-03-07 12:18:05 +0100

    Made A Working Duplication Check

--- student _manage.py
+++ student _manage.py
@@ -1,6 +1,7 @@
 student_list = []
 
 def get_students():
+    seen_names = set()
     print("type a students name")
     print("when you are done just press enter")
     while True:
@@ -9,6 +10,12 @@ def get_students():
 
         if name == "":
             break
+        if name in seen_names:
+            warning = input(f"{name} already exist are you sure you want to add it? y/n").strip().lower()
+            if warning == "n":
+                continue
+        seen_names.add(name)
+
         student_list.append({
             "name": name
         })
