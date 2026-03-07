c9940e15b293ef04aa412c4dce7243bbc1f46bf0
Linus Gustafsson <linus.gustaf01@gmail.com>
2026-03-07 12:50:36 +0100

    added Status to list working on state

--- student _manage.py
+++ student _manage.py
@@ -1,6 +1,7 @@
 student_list = []
 def get_students():
     seen_names = set()
+    state = True
     print("type a students name")
     print("when you are done just press enter")
     while True:
@@ -16,18 +17,21 @@ def get_students():
         seen_names.add(name)
 
         student_list.append({
-            "name": name
+            "name": name,
+            "status": state
         })
-    
-def set_student_state():
-    state = True
+    return name
+def set_student_state(name):
+    print("is there any students that are not present?")
+    print(student_list)
+    status = input("-> ")
 
-    #for student in student_list: #not working yet
-     
+
+
 
 def main ():
     get_students()
-    set_student_state()    
+    #set_student_state()    
     print(student_list)
 
 main()
