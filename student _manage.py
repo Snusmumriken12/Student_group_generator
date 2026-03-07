11003e5ba3f30bcdcf9b2154ffe15612582bface
Linus Gustafsson <linus.gustaf01@gmail.com>
2026-03-07 11:53:03 +0100

    broken name input loop

--- student _manage.py
+++ student _manage.py
@@ -1,5 +1,18 @@
 students = []
+name = None
 
-students = input("student name: ")
+num_student = input("how many students will you add? >")
+num_student = int(num_student)
+print("type a students name")
+print("when you are done just press enter")
+for i in num_student:
+    name = input("student name: ").lower().title
+
+    if name == "":
+        continue
+    students.append({
+        "name": name
+    })
+    
 
 print(students)
