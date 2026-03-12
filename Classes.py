94f7e4f203da161820f1f5621182c8885d299ae4
Linus Gustafsson <linus.gustaf01@gmail.com>
2026-03-12 10:56:49 +0100

    add: made a working create class loop

--- Classes.py
+++ Classes.py
@@ -1,7 +1,4 @@
-classes = {
-    "4a": [],
-    "5a": []
-}
+classes = {}
 
 def create_class(classes):
     class_name = input("Enter the name of the class -> ")
@@ -14,3 +11,27 @@ def create_class(classes):
 
     classes[class_name] = []
     print(f"Class '{class_name}' created.")
+
+while True:
+
+    print("\n1. Create Class ")
+    print("2. Show classes")
+    print("3. Exit")
+
+    choice = input("Choose-> ")
+    try:
+        choice = int(choice)
+    except ValueError:    
+        print("Value Error...")
+    
+    if choice == 1:
+        create_class(classes)
+    
+    elif choice == 2:
+        print(classes)
+    elif choice == 3:
+        break
+    else:
+        print("not a valid number")
+
+create_class(classes)
