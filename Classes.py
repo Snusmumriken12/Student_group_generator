9a0d86f2607a8376ac3b79462730cce30ddd756d
Linus Gustafsson <linus.gustaf01@gmail.com>
2026-03-12 11:09:03 +0100

    Stuck: dont know how to continue add: started making a rename_class funtion

--- Classes.py
+++ Classes.py
@@ -1,7 +1,7 @@
 classes = {}
 
 def create_class(classes):
-    class_name = input("Enter the name of the class -> ")
+    class_name = input("Enter the name of the class -> ").title().strip()
 
     if class_name == "":
         print("class name cannot be empty")
@@ -12,26 +12,41 @@ def create_class(classes):
     classes[class_name] = []
     print(f"Class '{class_name}' created.")
 
-while True:
+def rename_class(classes):
+   while True:
+    choice = input("What Class Name Do You Want To Rename? (Enter to exit)--> ").title().strip()
+    if choice == "":
+       break
+    elif choice in classes:
+        new_class_name = input("What Is The New Name? > ").title().strip()
 
-    print("\n1. Create Class ")
-    print("2. Show classes")
-    print("3. Exit")
-
-    choice = input("Choose-> ")
-    try:
-        choice = int(choice)
-    except ValueError:    
-        print("Value Error...")
     
-    if choice == 1:
-        create_class(classes)
-    
-    elif choice == 2:
-        print(classes)
-    elif choice == 3:
-        break
-    else:
-        print("not a valid number")
 
-create_class(classes)
+
+
+
+def main(classes):
+    while True:
+
+        print("\n1. Create Class ")
+        print("2. Show classes")
+        print("3. rename classes")
+        print("3. Exit")
+
+        choice = input("Choose-> ")
+        try:
+            choice = int(choice)
+        except ValueError:    
+            print("Value Error...")
+        
+        if choice == 1:
+            create_class(classes)
+        
+        elif choice == 2:
+            print(classes)
+        elif choice == 3:
+            break
+        else:
+            print("not a valid number")
+
+main(classes)
