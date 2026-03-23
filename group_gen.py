49cf40ad6236108017ecd9abbbd1209c61eb5b0b
Linus Gustafsson <linus.gustaf01@gmail.com>
2026-03-23 12:36:34 +0100

    feat: added so the user can choose between group size or number of groups

--- group_gen.py
+++ group_gen.py
@@ -19,22 +19,57 @@ def generate_group(classes):
             present_students.append(student["name"])
 
     random.shuffle(present_students)
-    while True:
-        group_size = input("how many per group -> ")
-        try:
-            group_size = int(group_size)
-        except ValueError:
-            print("ValueError")
-            return
-        groups = []
+    print("1. Group sizes")
+    print("2. Group total")
+    choice = input(">")
+    try:
+        choice = int(choice)
+    except ValueError:
+        print("ValueError")
+        return
+    if choice == 1:
+        while True:
+            group_size = input("how many per group -> ")
+            try:
+                group_size = int(group_size)
+            except ValueError:
+                print("ValueError")
+                return
+            groups = []
 
-        for i in range(0, len(present_students), group_size):
-            group = present_students[i:i+group_size]
-            groups.append(group)
-        
-        for i, group in enumerate(groups, start=1):
-            print(f"\nGroup {i}")
-            for student in group:
-                print("-", student)
+            for i in range(0, len(present_students), group_size):
+                group = present_students[i:i+group_size]
+                groups.append(group)
+            
+            for i, group in enumerate(groups, start=1):
+                print(f"\nGroup {i}")
+                for student in group:
+                    print("-", student)
+    elif choice == 2:
+        while True:
+            num_groups_input = input("How many groups--> ")
 
+            try:
+                num_groups = int(num_groups_input)
+            except ValueError:
+                print("Enter a number...")
+
+                if num_groups <= 0:
+                    print("Must be > 0")
+                    continue
+
+                if num_groups > len(present_students):
+                    print("More groups than students? Really?")
+                    continue
+
+                groups = [[] for _ in range(num_groups)]
+
+                for i, student in enumerate(present_students):
+                    groups[i % num_groups].append(student)
+
+               
+                for i, group in enumerate(groups, start=1):
+                    print(f"\nGroup {i}")
+                    for student in group:
+                        print("-", student)
 
